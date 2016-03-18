d8fbe237e37d03d89b25c213676419b5a2cbaa03
thalespc <thalespcosti@hotmail.com>
2016-03-18 18:07:37 -0300

    3.3 Led Azul+Verde+Vermelho - blink em cascata

--- Aula 4/GPIO_REGISTRADORES_LED/src/main.c
+++ Aula 4/GPIO_REGISTRADORES_LED/src/main.c
@@ -32,7 +32,8 @@
  * ----------------------------------
  */
 #define PIN_LED_BLUE 19
-
+#define PIN_LED_GREEN 20
+#define PIN_LED_RED 20
 /**
  * Main function
  * 1. configura o clock do sistema
@@ -60,22 +61,28 @@ int main (void)
 	// 29.17.4 PMC Peripheral Clock Enable Register 0
 	// 1: Enables the corresponding peripheral clock.
 	// ID_PIOA = 11 - TAB 11-1
-	PMC->PMC_PCER0 |= ID_PIOA;
+	
+	
+	PMC->PMC_PCER0 |= ID_PIOA | ID_PIOC; // initializing the peripherals
 
 	 //31.6.1 PIO Enable Register
 	// 1: Enables the PIO to control the corresponding pin (disables peripheral control of the pin).	
 	PIOA->PIO_PER |= (1 << PIN_LED_BLUE );
+	PIOA ->PIO_PER |= (1<< PIN_LED_GREEN);
+	PIOC ->PIO_PER |= (1<< PIN_LED_RED);
 
 	// 31.6.46 PIO Write Protection Mode Register
-	// 0: Disables the write protection if WPKEY corresponds to 0x50494F (PIO in ASCII).
+	// 0: Disables the write protection if WPKEY corresponds to 0x50494F (PIO in ASCII). 
+	//OS REGISTRADORES PODEM SER ALTERADOS
 	PIOA->PIO_WPMR = 0;
-	
+	PIOC->PIO_WPMR = 0;
 	// 31.6.4 PIO Output Enable Register
 	// value =
 	//	 	1 : Enables the output on the I/O line.
 	//	 	0 : do nothing
 	PIOA->PIO_OER |=  (1 << PIN_LED_BLUE );
-
+	PIOA->PIO_OER |=  (1 << PIN_LED_GREEN );
+	PIOC->PIO_OER |= (1 << PIN_LED_RED);
 	// 31.6.10 PIO Set Output Data Register
 	// value = 
 	// 		1 : Sets the data to be driven on the I/O line.
@@ -91,11 +98,18 @@ int main (void)
              * Utilize a função delay_ms para fazer o led piscar na frequência
              * escolhida por você.
              */
-			
+			PIOC->PIO_CODR = (1 << PIN_LED_RED );// sets the output to 1 and then the red led is turned off
+			delay_ms(500);
 			PIOA->PIO_SODR = (1 << PIN_LED_BLUE );// sets the output to 1 and then the blue led is turned off
-            delay_ms(1000);		
+			delay_ms(500);
+			PIOA->PIO_SODR = (1 << PIN_LED_GREEN);// sets the output to 1 and then the green led is turned off
+            delay_ms(300);		
+			PIOC->PIO_SODR = (1 << PIN_LED_RED );// sets the output to 0 and then the red led is turned on
+			delay_ms(500);
 			PIOA->PIO_CODR = (1 << PIN_LED_BLUE ); // sets the output to 0 and then the blue led is turned on
-			delay_ms(1000);
+			delay_ms(500);
+			PIOA->PIO_CODR = (1 << PIN_LED_GREEN);// sets the output to 0 and then the green led is turned on
+			delay_ms(300);
 	}
 }
 
