42203dc9dbc94e058a61c7472b95af226ddd0431
asa <as>
2016-04-22 18:37:20 -0300

    INTERRUPÇÃO

--- 11-Interruopcao/11 - INTERRUPCAO_PIO/src/main.c
+++ 11-Interruopcao/11 - INTERRUPCAO_PIO/src/main.c
@@ -13,7 +13,7 @@
  */
 
 static void push_button_handle(uint32_t id, uint32_t mask);
-
+static void push_button1_handle(uint32_t id, uint32_t mask);
 
 /** 
  * Definição dos pinos
@@ -37,6 +37,7 @@ static void push_button_handle(uint32_t id, uint32_t mask);
 #define PIN_LED_RED		20
 #define PIN_LED_GREEN	20
 #define PIN_BUTTON		3
+#define PIN_BUTTON_1	12	
 #define time			100
 
 /** 
@@ -47,6 +48,7 @@ static void push_button_handle(uint32_t id, uint32_t mask);
 #define PORT_LED_GREEN	PIOA
 #define PORT_LED_RED	PIOC
 #define PORT_BUT_2		PIOB
+#define PORT_BUT_1		PIOC
 
 /**
  * Define os IDs dos periféricos associados aos pinos
@@ -55,6 +57,7 @@ static void push_button_handle(uint32_t id, uint32_t mask);
 #define ID_LED_GREEN	ID_PIOA
 #define ID_LED_RED		ID_PIOC
 #define ID_BUT_2		ID_PIOB
+#define ID_BUT_1		ID_PIOC
 
 /**
  *	Define as masks utilziadas
@@ -63,13 +66,25 @@ static void push_button_handle(uint32_t id, uint32_t mask);
 #define MASK_LED_GREEN	(1u << PIN_LED_GREEN)
 #define MASK_LED_RED	(1u << PIN_LED_RED)
 #define MASK_BUT_2		(1u << PIN_BUTTON)
+#define MASK_BUT_1		(1u << PIN_BUTTON_1)
 
 /************************************************************************/
 /*  INTERRUPÇÃO PORTB													*/
 /************************************************************************/
 
 static void push_button_handle(uint32_t id, uint32_t mask){
-	pio_clear(PIOA, MASK_LED_GREEN);
+	if (PORT_LED_GREEN->PIO_ODSR & (1u << 20))
+		pio_clear(PIOA, MASK_LED_GREEN);
+	else
+		pio_set(PIOA, MASK_LED_GREEN);
+}
+
+static void push_button1_handle(uint32_t id, uint32_t mask){
+	if (PORT_LED_RED->PIO_ODSR & MASK_LED_RED)
+		pio_clear(PIOC, MASK_LED_RED);
+	else
+		pio_set(PIOC, MASK_LED_RED);
+	
 }
 
 
@@ -107,6 +122,7 @@ int main (void)
 	_pmc_enable_clock_periferico(ID_LED_GREEN);		// Redundante mas não tem problema !
 	_pmc_enable_clock_periferico(ID_LED_RED);
 	_pmc_enable_clock_periferico(ID_BUT_2);
+	_pmc_enable_clock_periferico(ID_BUT_1);
 				
 	/**
 	*	Configura saída
@@ -119,36 +135,49 @@ int main (void)
 	* Configura entrada
 	*/ 
 	pio_set_input(PORT_BUT_2, MASK_BUT_2, PIO_PULLUP | PIO_DEBOUNCE);
-	
+	pio_set_input(PORT_BUT_1, MASK_BUT_1, PIO_PULLUP | PIO_DEBOUNCE);
+
 	/*
 	 * Configura divisor do clock para debounce
 	 */
-	pio_set_debounce_filter(PORT_BUT_2, MASK_BUT_2, 100);
+	pio_set_debounce_filter(PORT_BUT_2, MASK_BUT_2, 20);
+	pio_set_debounce_filter(PORT_BUT_1, MASK_BUT_1, 20);
 	
 	/* 
 	*	Configura interrupção para acontecer em borda de descida.
 	*/
 	pio_handler_set(PORT_BUT_2, ID_BUT_2, MASK_BUT_2, PIO_IT_FALL_EDGE , push_button_handle );
+	pio_handler_set(PORT_BUT_1, ID_BUT_1, MASK_BUT_1, PIO_IT_FALL_EDGE , push_button1_handle );
 				
 	/*
 	*	Ativa interrupção no periférico B porta do botão
 	*/	
 	pio_enable_interrupt(PORT_BUT_2, MASK_BUT_2);
+	pio_enable_interrupt(PORT_BUT_1, MASK_BUT_1);
 	
 	/*
 	*	Configura a prioridade da interrupção no pORTB
 	*/
 	NVIC_SetPriority((IRQn_Type) ID_PIOB, 2);
-	
+	NVIC_SetPriority((IRQn_Type) ID_PIOC, 2);
+
 	/*
 	*	Ativa interrupção no port B
 	*/
 	NVIC_EnableIRQ((IRQn_Type) ID_PIOB);
-	
+	NVIC_EnableIRQ((IRQn_Type) ID_PIOC);
+
 	/**
 	*	Loop infinito
 	*/
 	while(1){
+
+		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
+
+		pio_set(PIOA, (1 << PIN_LED_BLUE));
+		delay_ms(500);
+		pio_clear(PIOA, (1 << PIN_LED_BLUE));
+		delay_ms(500);
 		pio_set(PIOA, (1 << PIN_LED_BLUE));
 		delay_ms(500);
 		pio_clear(PIOA, (1 << PIN_LED_BLUE));
