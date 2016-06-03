0be18479aefc89ca0fc64bb40846f4a742225d05
ghfjyg <fgzgfzgf>
2016-06-03 18:33:59 -0300

    aula 15 - dac
    senoide

--- 11 - INTERRUPCAO_PIO/src/main.c
+++ 11 - INTERRUPCAO_PIO/src/main.c
@@ -1,187 +1,394 @@
-/**
- * IMT - Rafael Corsi
- * 
- * Interrupção
-*/
+94 lines (321 sloc)  11.1 KB
+#include "asf.h"
+#include "stdio_serial.h"
+#include "conf_board.h"
+#include "conf_clock.h"
+#include "smc.h"
+#include "img.h"
+#include <math.h>
 
-#include <asf.h>
-#include "Driver/pio_maua.h"
-#include "Driver/pmc_maua.h"
+/** Chip select number to be set */
+#define ILI93XX_LCD_CS      1
 
-/*
- * Prototypes
- */
+struct ili93xx_opt_t g_ili93xx_display_opt;
 
-static void push_button_handle(uint32_t id, uint32_t mask);
-static void push_button1_handle(uint32_t id, uint32_t mask);
-
-/** 
- * Definição dos pinos
- * Pinos do uC referente aos LEDS/ Botão
- *
- * O número referente ao pino (PIOAxx), refere-se ao
- * bit que deve ser configurado no registrador para alterar
- * o estado desse bit específico.
- *
- * exe : O pino PIOA_19 é configurado nos registradores pelo bit
- * 19. O registrador PIO_SODR configura se os pinos serão nível alto.
- * Nesse caso o bit 19 desse registrador é referente ao pino PIOA_19
- *
- * ----------------------------------
- * | BIT 19  | BIT 18  | ... |BIT 0 |
- * ----------------------------------
- * | PIOA_19 | PIOA_18 | ... |PIOA_0|
- * ----------------------------------
- */
-#define PIN_LED_BLUE	19
-#define PIN_LED_RED		20
-#define PIN_LED_GREEN	20
-#define PIN_BUTTON		3
-#define PIN_BUTTON_1	12	
-#define time			100
-
-/** 
- * Definição dos ports
- * Ports referentes a cada pino
- */
-#define PORT_LED_BLUE	PIOA
-#define PORT_LED_GREEN	PIOA
-#define PORT_LED_RED	PIOC
-#define PORT_BUT_2		PIOB
-#define PORT_BUT_1		PIOC
-
-/**
- * Define os IDs dos periféricos associados aos pinos
- */
-#define ID_LED_BLUE		ID_PIOA
-#define ID_LED_GREEN	ID_PIOA
-#define ID_LED_RED		ID_PIOC
-#define ID_BUT_2		ID_PIOB
-#define ID_BUT_1		ID_PIOC
-
-/**
- *	Define as masks utilziadas
- */
-#define MASK_LED_BLUE	(1u << PIN_LED_BLUE)
-#define MASK_LED_GREEN	(1u << PIN_LED_GREEN)
-#define MASK_LED_RED	(1u << PIN_LED_RED)
-#define MASK_BUT_2		(1u << PIN_BUTTON)
-#define MASK_BUT_1		(1u << PIN_BUTTON_1)
+#define PIN_PUSHBUTTON_1_MASK	PIO_PB3
+#define PIN_PUSHBUTTON_1_PIO	PIOB
+#define PIN_PUSHBUTTON_1_ID		ID_PIOB
+#define PIN_PUSHBUTTON_1_TYPE	PIO_INPUT
+#define PIN_PUSHBUTTON_1_ATTR	PIO_PULLUP | PIO_DEBOUNCE | PIO_IT_FALL_EDGE
 
 /************************************************************************/
-/*  INTERRUPÇÃO PORTB													*/
+/* ADC                                                                     */
 /************************************************************************/
 
-static void push_button_handle(uint32_t id, uint32_t mask){
-	if (PORT_LED_GREEN->PIO_ODSR & (1u << 20))
-		pio_clear(PIOA, MASK_LED_GREEN);
-	else
-		pio_set(PIOA, MASK_LED_GREEN);
+/** Size of the receive buffer and transmit buffer. */
+#define BUFFER_SIZE     (100)
+/** Reference voltage for ADC,in mv. */
+#define VOLT_REF        (3300)
+/* Tracking Time*/
+#define TRACKING_TIME    1
+/* Transfer Period */
+#define TRANSFER_PERIOD  1
+/* Startup Time*/
+#define STARTUP_TIME ADC_STARTUP_TIME_4
+
+/** The maximal digital value */
+#define MAX_DIGITAL     (4095)
+
+/* Redefinir isso */
+#define ADC_POT_CHANNEL 5
+
+/** adc buffer */
+static int16_t gs_s_adc_values[BUFFER_SIZE] = { 0 };
+
+/************************************************************************/
+/* GLOBAL                                                                */
+/************************************************************************/
+uint32_t adc_value_old;
+bool isAmp = true;
+uint16_t i = 0;
+uint32_t amplitude = 0;
+uint32_t frequencia = 0;
+
+//! DAC channel used for test
+#define DACC_CHANNEL        1 // (PB14)
+
+//! DAC register base for test
+#define DACC_BASE           DACC
+//! DAC ID for test
+#define DACC_ID             ID_DACC
+
+/** Analog control value */
+#define DACC_ANALOG_CONTROL (DACC_ACR_IBCTLCH0(0x02) \
+| DACC_ACR_IBCTLCH1(0x02) \
+| DACC_ACR_IBCTLDACCORE(0x01))
+
+/** The analog voltage reference **/
+#define VADREF    (float) 3.3
+/** The maximal data value (no sign) */
+#define MAX_DIGITAL ((1 << DACC_RESOLUTION) - 1)
+/** The maximal (peak-peak) amplitude value */
+#define MAX_AMPLITUDE (float) 5/6
+/** The minimal (peak-peak) amplitude value */
+#define MIN_AMPLITUDE (float) 1/6
+
+uint32_t desenhar_seno(uint16_t i) {
+	float pontos = (2*M_PI)/MAX_DIGITAL*((float) i);
+	return amplitude/2*sin(pontos) + MAX_DIGITAL/2;
 }
 
-static void push_button1_handle(uint32_t id, uint32_t mask){
-	if (PORT_LED_RED->PIO_ODSR & MASK_LED_RED)
-		pio_clear(PIOC, MASK_LED_RED);
-	else
-		pio_set(PIOC, MASK_LED_RED);
-	
-}
-
-
-/**
- * Main function
- * 1. configura o clock do sistema
- * 2. desabilita wathdog
- * 3. Configura os LEDs como saída
- * 4. Configura o botão como entrada
- * 5. Configura a interrupção no botão
- * 6. Configura a interrupção no NVIC/CORE
- * 7. Ativa interrupção
- * 8. while(1)
- * 
- * 9. Quando o botão for apertado, a função push_button_handle é chamada via interrupção
- */
-
-int main (void)
+static void configure_tc(uint32_t freq)
 {
-
-	/**
-	* Inicializando o clock do uP
-	*/
-	sysclk_init();
-	
-	/** 
-	*  Desabilitando o WathDog do uP
-	*/
-	WDT->WDT_MR = WDT_MR_WDDIS;
-		
-	/**
-	* Ativa clock nos periféricos
-	*/
-	_pmc_enable_clock_periferico(ID_LED_BLUE);
-	_pmc_enable_clock_periferico(ID_LED_GREEN);		// Redundante mas não tem problema !
-	_pmc_enable_clock_periferico(ID_LED_RED);
-	_pmc_enable_clock_periferico(ID_BUT_2);
-	_pmc_enable_clock_periferico(ID_BUT_1);
-				
-	/**
-	*	Configura saída
-	*/
-	pio_set_output(PORT_LED_BLUE  , MASK_LED_BLUE	,1,0,0);
-	pio_set_output(PORT_LED_GREEN , MASK_LED_GREEN  ,1,0,0);
-	pio_set_output(PORT_LED_RED	  , MASK_LED_RED	,1,0,0);
-	
-	/**
-	* Configura entrada
-	*/ 
-	pio_set_input(PORT_BUT_2, MASK_BUT_2, PIO_PULLUP | PIO_DEBOUNCE);
-	pio_set_input(PORT_BUT_1, MASK_BUT_1, PIO_PULLUP | PIO_DEBOUNCE);
-
 	/*
-	 * Configura divisor do clock para debounce
-	 */
-	pio_set_debounce_filter(PORT_BUT_2, MASK_BUT_2, 20);
-	pio_set_debounce_filter(PORT_BUT_1, MASK_BUT_1, 20);
-	
-	/* 
-	*	Configura interrupção para acontecer em borda de descida.
+	* Aqui atualizamos o clock da cpu que foi configurado em sysclk init
+	*
+	* O valor atual está em : 120_000_000 Hz (120Mhz)
 	*/
-	pio_handler_set(PORT_BUT_2, ID_BUT_2, MASK_BUT_2, PIO_IT_FALL_EDGE , push_button_handle );
-	pio_handler_set(PORT_BUT_1, ID_BUT_1, MASK_BUT_1, PIO_IT_FALL_EDGE , push_button1_handle );
-				
-	/*
-	*	Ativa interrupção no periférico B porta do botão
-	*/	
-	pio_enable_interrupt(PORT_BUT_2, MASK_BUT_2);
-	pio_enable_interrupt(PORT_BUT_1, MASK_BUT_1);
+	uint32_t ul_sysclk = sysclk_get_cpu_hz();
 	
 	/*
-	*	Configura a prioridade da interrupção no pORTB
+	*	Ativa o clock do periférico TC 0
 	*/
-	NVIC_SetPriority((IRQn_Type) ID_PIOB, 2);
-	NVIC_SetPriority((IRQn_Type) ID_PIOC, 2);
+	pmc_enable_periph_clk(ID_TC0);
+	
+	// Configura TC para operar no modo de comparação e trigger RC
+	
+	tc_init(TC0,0,TC_CMR_CPCTRG | TC_CMR_TCCLKS_TIMER_CLOCK5);
 
-	/*
-	*	Ativa interrupção no port B
-	*/
-	NVIC_EnableIRQ((IRQn_Type) ID_PIOB);
-	NVIC_EnableIRQ((IRQn_Type) ID_PIOC);
+	// Valor para o contador de um em um segundo.
+	tc_write_rc(TC0,0, (uint32_t) freq);
+	//tc_write_rc(TC0,0,65536);
 
-	/**
-	*	Loop infinito
-	*/
-	while(1){
+	NVIC_EnableIRQ((IRQn_Type) ID_TC0);
+	
+	tc_enable_interrupt(TC0,0,TC_IER_CPCS);
+	
+	tc_start(TC0, 0);
+}
 
-		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
+/**
+*  Interrupt handler for TC0 interrupt.
+*/
+void TC0_Handler(void){
+  volatile uint32_t ul_dummy, status;
+  uint32_t valorDAC = 1024;
+  ul_dummy = tc_get_status(TC0,0);
+  UNUSED(ul_dummy);
+  
+  /************************************************************************/
+  /* Escreve um novo valor no DAC                                         */
+  /************************************************************************/
+  //status = dacc_get_interrupt_status(DACC_BASE);
+  //dacc_write_conversion_data(DACC_BASE, valorDAC);
+  
+  if (i <= MAX_DIGITAL) {
+	   dacc_write_conversion_data(DACC_BASE, desenhar_seno(i++));
+	   } else {
+	   i = 0;
+   }
+}
 
-		pio_set(PIOA, (1 << PIN_LED_BLUE));
-		delay_ms(500);
-		pio_clear(PIOA, (1 << PIN_LED_BLUE));
-		delay_ms(500);
-		pio_set(PIOA, (1 << PIN_LED_BLUE));
-		delay_ms(500);
-		pio_clear(PIOA, (1 << PIN_LED_BLUE));
-		delay_ms(500);
+
+
+void atualiza_freq(uint32_t val){
+	ili93xx_set_foreground_color(COLOR_WHITE);
+	ili93xx_draw_filled_rectangle(140,48,200,100);
+	ili93xx_set_foreground_color(COLOR_BLACK);
+	char buffer[10];
+	snprintf(buffer, 10, "%d", val);
+	ili93xx_draw_string(145, 50, (uint8_t *)buffer);
+}
+
+void atualiza_amp(uint32_t val){
+	ili93xx_set_foreground_color(COLOR_WHITE);
+	ili93xx_draw_filled_rectangle(125,8,200,40);
+	ili93xx_set_foreground_color(COLOR_BLACK);
+	char buffer[10];
+	snprintf(buffer, 10, "%d", val);
+	ili93xx_draw_string(130, 10, (uint8_t *)buffer);
+}
+
+/**
+ * \brief ADC interrupt handler.
+ * Entramos aqui quando a conversao for concluida.
+ */
+void ADC_Handler(void)
+{
+	uint32_t tempValue;
+	uint32_t status ;
+
+	status = adc_get_status(ADC);
+	
+	/* Checa se a interrupção é devido ao canal 5 */
+	if ((status & ADC_ISR_EOC5)) {
+		tempValue = adc_get_channel_value(ADC, ADC_POT_CHANNEL);
+		if ((tempValue > adc_value_old + 2) || (tempValue < adc_value_old - 2))
+		{
+			if (isAmp){
+				amplitude = tempValue;
+				atualiza_amp(amplitude);
+			} else {
+				frequencia = tempValue;
+				tc_stop(TC0,0);
+				configure_tc((frequencia*32768/2000)/4095);
+				atualiza_freq(frequencia);
+			}
+		}
+		adc_value_old = tempValue;
+	}
+	
+}
+
+void atualiza_variavel() {
+	ili93xx_set_foreground_color(COLOR_WHITE);
+	ili93xx_draw_filled_rectangle(50,100,200,170);
+	ili93xx_set_foreground_color(COLOR_BLACK);
+	if (isAmp) {
+		ili93xx_draw_string(60, 110, (uint8_t *) "Amplitude");
+	} else {
+		ili93xx_draw_string(60, 110, (uint8_t *) "Frequencia");
 	}
 }
 
+/**
+ *  Handle Interrupcao botao 1.
+ *  Decrementa o contador e atualiza o display.
+ */
+static void Button1_Handler(uint32_t id, uint32_t mask) {
+	if (isAmp) {
+		isAmp = false;
+		atualiza_variavel();
+	} else {
+		isAmp = true;
+		atualiza_variavel();
+	}
+}
+
+/**
+ *  Handle Interrupcao botao 1.
+ *  Decrementa o contador e atualiza o display.
+ */
+static void Button2_Handler(uint32_t id, uint32_t mask) {
+	adc_start(ADC);
+}
+
+void configure_LCD(void){
+  /** Enable peripheral clock */
+  pmc_enable_periph_clk(ID_SMC);
+
+  /** Configure SMC interface for Lcd */
+  smc_set_setup_timing(SMC, ILI93XX_LCD_CS, SMC_SETUP_NWE_SETUP(2)
+  | SMC_SETUP_NCS_WR_SETUP(2)
+  | SMC_SETUP_NRD_SETUP(2)
+  | SMC_SETUP_NCS_RD_SETUP(2));
+  smc_set_pulse_timing(SMC, ILI93XX_LCD_CS, SMC_PULSE_NWE_PULSE(4)
+  | SMC_PULSE_NCS_WR_PULSE(4)
+  | SMC_PULSE_NRD_PULSE(10)
+  | SMC_PULSE_NCS_RD_PULSE(10));
+  smc_set_cycle_timing(SMC, ILI93XX_LCD_CS, SMC_CYCLE_NWE_CYCLE(10)
+  | SMC_CYCLE_NRD_CYCLE(22));
+  #if ((!defined(SAM4S)) && (!defined(SAM4E)))
+  smc_set_mode(SMC, ILI93XX_LCD_CS, SMC_MODE_READ_MODE
+  | SMC_MODE_WRITE_MODE
+  | SMC_MODE_DBW_8_BIT);
+  #else
+  smc_set_mode(SMC, ILI93XX_LCD_CS, SMC_MODE_READ_MODE
+  | SMC_MODE_WRITE_MODE);
+  #endif
+  /** Initialize display parameter */
+  g_ili93xx_display_opt.ul_width = ILI93XX_LCD_WIDTH;
+  g_ili93xx_display_opt.ul_height = ILI93XX_LCD_HEIGHT;
+  g_ili93xx_display_opt.foreground_color = COLOR_BLACK;
+  g_ili93xx_display_opt.background_color = COLOR_WHITE;
+
+  /** Switch off backlight */
+  aat31xx_disable_backlight();
+
+  /** Initialize LCD */
+  ili93xx_init(&g_ili93xx_display_opt);
+
+  /** Set backlight level */
+  aat31xx_set_backlight(AAT31XX_AVG_BACKLIGHT_LEVEL);
+
+  ili93xx_set_foreground_color(COLOR_WHITE);
+  ili93xx_draw_filled_rectangle(0, 0, ILI93XX_LCD_WIDTH,
+  ILI93XX_LCD_HEIGHT);
+  /** Turn on LCD */
+  ili93xx_display_on();
+  ili93xx_set_cursor_position(0, 0);
+};
+
+void configure_DAC() {
+	 /************************************************************************/
+	 /* DAC                                                                  */
+	 /************************************************************************/
+	 
+	 /* Enable clock for DACC */
+	 sysclk_enable_peripheral_clock(DACC_ID);
+	 
+	 /* Reset DACC registers */
+	 dacc_reset(DACC_BASE);
+
+	 /* Half word transfer mode */
+	 dacc_set_transfer_mode(DACC_BASE, 0);
+
+	 /* selects channel */
+	 dacc_set_channel_selection(DACC_BASE, DACC_CHANNEL);
+
+	 /* Enable output channel DACC_CHANNEL */
+	 dacc_enable_channel(DACC_BASE, DACC_CHANNEL);
+
+	 /* Set up analog current */
+	 dacc_set_analog_control(DACC_BASE, DACC_ANALOG_CONTROL);
+}
+
+void configure_ADC(void){
+	
+	/* Enable peripheral clock. */
+	pmc_enable_periph_clk(ID_ADC);
+	/* Initialize ADC. */
+	/*
+	* Formula: ADCClock = MCK / ( (PRESCAL+1) * 2 )
+	* For example, MCK = 64MHZ, PRESCAL = 4, then:
+	* ADCClock = 64 / ((4+1) * 2) = 6.4MHz;
+	*/
+	/* Formula:
+	*     Startup  Time = startup value / ADCClock
+	*     Startup time = 64 / 6.4MHz = 10 us
+	*/
+	adc_init(ADC, sysclk_get_cpu_hz(), 6400000, STARTUP_TIME);
+	/* Formula:
+	*     Transfer Time = (TRANSFER * 2 + 3) / ADCClock
+	*     Tracking Time = (TRACKTIM + 1) / ADCClock
+	*     Settling Time = settling value / ADCClock
+	*
+	*     Transfer Time = (1 * 2 + 3) / 6.4MHz = 781 ns
+	*     Tracking Time = (1 + 1) / 6.4MHz = 312 ns
+	*     Settling Time = 3 / 6.4MHz = 469 ns
+	*/
+	adc_configure_timing(ADC, TRACKING_TIME	, ADC_SETTLING_TIME_3, TRANSFER_PERIOD);
+
+	adc_configure_trigger(ADC, ADC_TRIG_SW, 0);
+
+	//adc_check(ADC, sysclk_get_cpu_hz());
+
+	/* Enable channel for potentiometer. */
+	adc_enable_channel(ADC, ADC_POT_CHANNEL);
+
+	/* Enable the temperature sensor. */
+	adc_enable_ts(ADC);
+
+	/* Enable ADC interrupt. */
+	NVIC_EnableIRQ(ADC_IRQn);
+
+	/* Start conversion. */
+	adc_start(ADC);
+
+	//adc_read_buffer(ADC, gs_s_adc_values, BUFFER_SIZE);
+
+	//adc_get_channel_value(ADC, ADC_POT_CHANNEL);
+
+	/* Enable PDC channel interrupt. */
+	adc_enable_interrupt(ADC, ADC_ISR_EOC5);
+}
+
+/**
+ *  Configure Timer Counter 0 to generate an interrupt every 1s.
+ */
+
+static void configure_Button() 
+{
+	pmc_enable_periph_clk(PIN_PUSHBUTTON_1_ID);
+	pmc_enable_periph_clk(PIN_PUSHBUTTON_2_ID);
+	
+	pio_set_input(PIN_PUSHBUTTON_1_PIO, PIN_PUSHBUTTON_1_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_input(PIN_PUSHBUTTON_2_PIO, PIN_PUSHBUTTON_2_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+
+	pio_set_debounce_filter(PIN_PUSHBUTTON_1_PIO, PIN_PUSHBUTTON_1_MASK, 10);
+	pio_set_debounce_filter(PIN_PUSHBUTTON_2_PIO, PIN_PUSHBUTTON_2_MASK, 10);
+	
+	pio_handler_set(PIN_PUSHBUTTON_1_PIO, PIN_PUSHBUTTON_1_ID, PIN_PUSHBUTTON_1_MASK, PIO_IT_FALL_EDGE | PIO_PULLUP, Button1_Handler);
+	pio_handler_set(PIN_PUSHBUTTON_2_PIO, PIN_PUSHBUTTON_2_ID, PIN_PUSHBUTTON_2_MASK, PIO_IT_FALL_EDGE | PIO_PULLUP, Button2_Handler);
+	
+	pio_enable_interrupt(PIN_PUSHBUTTON_1_PIO, PIN_PUSHBUTTON_1_MASK);
+	pio_enable_interrupt(PIN_PUSHBUTTON_2_PIO, PIN_PUSHBUTTON_2_MASK);
+
+	NVIC_SetPriority((IRQn_Type) PIN_PUSHBUTTON_1_ID, 0);
+	NVIC_SetPriority((IRQn_Type) PIN_PUSHBUTTON_2_ID, 0);
+	
+	NVIC_EnableIRQ((IRQn_Type) PIN_PUSHBUTTON_1_ID);
+	NVIC_EnableIRQ((IRQn_Type) PIN_PUSHBUTTON_2_ID);
+}
+
+/**
+* \brief Application entry point for smc_lcd example.
+*
+* \return Unused (ANSI-C compatibility).
+*/
+int main(void)
+{
+  sysclk_init();
+  board_init();
+
+  configure_LCD();
+  configure_DAC();
+  configure_ADC();
+  configure_tc(4095);
+  configure_Button();
+  ili93xx_set_foreground_color(COLOR_BLACK);
+  ili93xx_draw_string(60, 110, (uint8_t *) "Amplitude");
+  ili93xx_draw_string(10, 10, (uint8_t *) "Amplitude:");
+  ili93xx_draw_string(10, 50, (uint8_t *) "Frequencia:");
+  atualiza_freq(frequencia);
+  atualiza_amp(amplitude);
+
+  ili93xx_draw_pixmap(0,
+            ILI93XX_LCD_HEIGHT-100-1,
+            240-1,
+            100-1,
+            &image_data_maua[0]);
+
+  while (1) {
+  }
+}
---
+++ 15-DAC/LCD-Desenhos/src/ili93xx_example.c
@@ -0,0 +1,365 @@
+#include "asf.h"
+#include "stdio_serial.h"
+#include "conf_board.h"
+#include "conf_clock.h"
+#include "smc.h"
+#include "img.h"
+#include "arm_math.h"
+
+/** Chip select number to be set */
+#define ILI93XX_LCD_CS      1
+
+struct ili93xx_opt_t g_ili93xx_display_opt;
+
+//! DAC channel used for test
+#define DACC_CHANNEL        1 // (PB14)
+
+#define ADC_POT_CHANNEL 5
+//! DAC register base for test
+#define DACC_BASE           DACC
+//! DAC ID for test
+#define DACC_ID             ID_DACC
+
+/** Size of the receive buffer and transmit buffer. */
+#define BUFFER_SIZE     (100)
+/** Reference voltage for ADC,in mv. */
+#define VOLT_REF        (3300)
+/* Tracking Time*/
+#define TRACKING_TIME    1
+/* Transfer Period */
+#define TRANSFER_PERIOD  1
+/* Startup Time*/
+#define STARTUP_TIME ADC_STARTUP_TIME_4
+
+
+
+/** Analog control value */
+#define DACC_ANALOG_CONTROL (DACC_ACR_IBCTLCH0(0x02) \
+| DACC_ACR_IBCTLCH1(0x02) \
+| DACC_ACR_IBCTLDACCORE(0x01))
+
+/** The analog voltage reference **/
+#define VADREF    (float) 3.3
+/** The maximal data value (no sign) */
+#define MAX_DIGITAL   ((1 << DACC_RESOLUTION) - 1)
+/** The maximal (peak-peak) amplitude value */
+#define MAX_AMPLITUDE_ANAG (float) 5/6*VADREF
+/** The minimal (peak-peak) amplitude value */
+#define MIN_AMPLITUDE_ANAG (float) 1/6*VADREF
+
+/**
+ * global variables
+ */
+
+int namostra = 0 ;
+float32_t ySeno;
+int resolucao	;
+float deltaTeta ;
+float valorMedio;
+int amplitude;
+float rad;
+int nleituraADC = 0;
+int max_digital;
+
+/**
+*  Interrupt handler for TC0 interrupt.
+*/
+void ADC_Handler(void)
+{
+	uint32_t tmp;
+	uint32_t status ;
+
+	status = adc_get_status(ADC);
+	max_digital = adc_get_channel_value(ADC, ADC_POT_CHANNEL);
+}
+	
+void TC0_Handler(void){
+
+  volatile uint32_t ul_dummy, status;
+  uint32_t valorDAC = 1024;
+  ul_dummy = tc_get_status(TC0,0);
+  UNUSED(ul_dummy);
+  
+
+  /************************************************************************/
+  /* ADC                                                                     */
+  /************************************************************************/
+  if(nleituraADC >= 500){
+	 adc_start(ADC);
+	 nleituraADC = 0;
+   }
+   nleituraADC++;
+
+  /************************************************************************/
+  /* Escreve um novo valor no DAC                                         */
+  /************************************************************************/
+  status = dacc_get_interrupt_status(DACC_BASE);
+
+  /* namostra > 2*pi  ?? */
+  if(namostra > resolucao)
+	namostra = 0;
+
+  ySeno = (sin(deltaTeta*namostra)+1)*((float)max_digital)/MAX_AMPLITUDE_ANAG;
+  dacc_write_conversion_data(DACC_BASE, ySeno);
+
+  namostra++;
+
+ 
+}
+
+static void configure_tc(int freq)
+{
+	/****************************************************************
+	* Devemos indicar ao TC que a interrupção foi satisfeita.
+	******************************************************************/
+	
+	// [main_tc_configure]
+	/*
+	* Aqui atualizamos o clock da cpu que foi configurado em sysclk init
+	*
+	* O valor atual est'a em : 120_000_000 Hz (120Mhz)
+	*/
+	uint32_t ul_sysclk = sysclk_get_cpu_hz();
+	
+	/****************************************************************
+	* Ativa o clock do periférico TC 0
+	*****************************************************************
+	*
+	* Parametros :
+	*  1 - ID do periferico
+	*
+	*
+	*****************************************************************/
+	pmc_enable_periph_clk(ID_TC0);
+
+	/*****************************************************************
+	* Configura TC para operar no modo de comparação e trigger RC
+	*****************************************************************
+	*
+	* Configura TC para operar no modo de comparação e trigger RC
+	* devemos nos preocupar com o clock em que o TC irá operar !
+	*
+	* Cada TC possui 3 canais, escolher um para utilizar.
+	*
+	* No nosso caso :
+	
+	*
+	*	MCK		= 120_000_000
+	*	SLCK	= 32_768		(rtc)
+	*
+	* Uma opção para achar o valor do divisor é utilizar a funcao, como ela
+	* funciona ?
+	* tc_find_mck_divisor()
+	*
+	*
+	* Parametros
+	*   1 - TC a ser configurado (TC0,TC1, ...)
+	*   2 - Canal a ser configurado (0,1,2)
+	*   3 - Configurações do TC :
+	*
+	*	* Configurações de modo de operação :
+	*	    TC_CMR_ABETRG  : TIOA or TIOB External Trigger Selection
+	*	    TC_CMR_CPCTRG  : RC Compare Trigger Enable
+	*	    TC_CMR_WAVE    : Waveform Mode
+	*
+	*     Configurações de clock :
+	*	    TC_CMR_TCCLKS_TIMER_CLOCK1 : Clock selected: internal MCK/2 clock signal
+	*	    TC_CMR_TCCLKS_TIMER_CLOCK2 : Clock selected: internal MCK/8 clock signal
+	*	    TC_CMR_TCCLKS_TIMER_CLOCK3 : Clock selected: internal MCK/32 clock signal
+	*	    TC_CMR_TCCLKS_TIMER_CLOCK4 : Clock selected: internal MCK/128 clock signal
+	*	    TC_CMR_TCCLKS_TIMER_CLOCK5 : Clock selected: internal SLCK clock signal
+	*
+	*****************************************************************/
+	tc_init(TC0, 0, TC_CMR_CPCTRG | TC_CMR_TCCLKS_TIMER_CLOCK5);
+	
+	/*****************************************************************
+	* Configura valor trigger RC
+	*****************************************************************
+	*
+	* Aqui devemos configurar o valor do RC que vai trigar o reinicio da contagem
+	* devemos levar em conta a frequência que queremos que o TC gere as interrupções
+	* e tambem a frequencia com que o TC está operando.
+	*
+	* Devemos configurar o RC para o mesmo canal escolhido anteriormente.
+	*
+	*   ^
+	*	|	Contador (incrementado na frequencia escolhida do clock)
+	*   |
+	*	|	 	Interrupcao
+	*	|------#----------- RC
+	*	|	  /
+	*	|   /
+	*	| /
+	*	|-----------------> t
+	*
+	* Parametros :
+	*   1 - TC a ser configurado (TC0,TC1, ...)
+	*   2 - Canal a ser configurado (0,1,2)
+	*   3 - Valor para trigger do contador (RC)
+	*****************************************************************/
+	tc_write_rc(TC0,0,32768/freq);
+	
+	/*****************************************************************
+	* Configura interrupção no TC
+	*****************************************************************
+	* Parametros :
+	*   1 - TC a ser configurado
+	*   2 - Canal
+	*   3 - Configurações das interrupções
+	*
+	*        Essas configurações estão definidas no head : tc.h
+	*
+	*	        TC_IER_COVFS : 	Counter Overflow
+	*	        TC_IER_LOVRS : 	Load Overrun
+	*	        TC_IER_CPAS  : 	RA Compare
+	*	        TC_IER_CPBS  : 	RB Compare
+	*	        TC_IER_CPCS  : 	RC Compare
+	*	        TC_IER_LDRAS : 	RA Loading
+	*	        TC_IER_LDRBS : 	RB Loading
+	*	        TC_IER_ETRGS : 	External Trigger
+	*****************************************************************/
+	tc_enable_interrupt(TC0,0,TC_IER_CPCS);
+	
+	/*****************************************************************
+	* Ativar interrupção no NVIC*/
+	
+	
+	NVIC_EnableIRQ(ID_TC0);
+	
+	/*****************************************************************
+	* Inicializa o timer
+	*****************************************************************
+	*
+	* Parametros :
+	*   1 - TC
+	*   2 - Canal
+	*****************************************************************/
+	tc_start(TC0,0);
+
+
+	/************************************************************************/
+	/* Main Code
+	*/
+	/************************************************************************/
+}
+
+void configure_LCD(void){
+  /** Enable peripheral clock */
+  pmc_enable_periph_clk(ID_SMC);
+
+  /** Configure SMC interface for Lcd */
+  smc_set_setup_timing(SMC, ILI93XX_LCD_CS, SMC_SETUP_NWE_SETUP(2)
+  | SMC_SETUP_NCS_WR_SETUP(2)
+  | SMC_SETUP_NRD_SETUP(2)
+  | SMC_SETUP_NCS_RD_SETUP(2));
+  smc_set_pulse_timing(SMC, ILI93XX_LCD_CS, SMC_PULSE_NWE_PULSE(4)
+  | SMC_PULSE_NCS_WR_PULSE(4)
+  | SMC_PULSE_NRD_PULSE(10)
+  | SMC_PULSE_NCS_RD_PULSE(10));
+  smc_set_cycle_timing(SMC, ILI93XX_LCD_CS, SMC_CYCLE_NWE_CYCLE(10)
+  | SMC_CYCLE_NRD_CYCLE(22));
+  #if ((!defined(SAM4S)) && (!defined(SAM4E)))
+  smc_set_mode(SMC, ILI93XX_LCD_CS, SMC_MODE_READ_MODE
+  | SMC_MODE_WRITE_MODE
+  | SMC_MODE_DBW_8_BIT);
+  #else
+  smc_set_mode(SMC, ILI93XX_LCD_CS, SMC_MODE_READ_MODE
+  | SMC_MODE_WRITE_MODE);
+  #endif
+  /** Initialize display parameter */
+  g_ili93xx_display_opt.ul_width = ILI93XX_LCD_WIDTH;
+  g_ili93xx_display_opt.ul_height = ILI93XX_LCD_HEIGHT;
+  g_ili93xx_display_opt.foreground_color = COLOR_BLACK;
+  g_ili93xx_display_opt.background_color = COLOR_WHITE;
+
+  /** Switch off backlight */
+  aat31xx_disable_backlight();
+
+  /** Initialize LCD */
+  ili93xx_init(&g_ili93xx_display_opt);
+
+  /** Set backlight level */
+  aat31xx_set_backlight(AAT31XX_AVG_BACKLIGHT_LEVEL);
+
+  ili93xx_set_foreground_color(COLOR_WHITE);
+  ili93xx_draw_filled_rectangle(0, 0, ILI93XX_LCD_WIDTH,
+  ILI93XX_LCD_HEIGHT);
+  /** Turn on LCD */
+  ili93xx_display_on();
+  ili93xx_set_cursor_position(0, 0);
+};
+
+
+void configure_adc(void)
+{
+	/* Enable peripheral clock. */
+	pmc_enable_periph_clk(ID_ADC);
+	adc_init(ADC, sysclk_get_cpu_hz(), 6400000, STARTUP_TIME);
+	adc_configure_timing(ADC, TRACKING_TIME	, ADC_SETTLING_TIME_3, TRANSFER_PERIOD);
+	adc_configure_trigger(ADC, ADC_TRIG_SW, 0);
+	/* Enable chnnel for potentiometer. */
+	adc_enable_channel(ADC, ADC_POT_CHANNEL);
+	/* Enable ADC interrupt. */
+	NVIC_EnableIRQ(ADC_IRQn);
+	/* Start conversion. */
+	adc_start(ADC);
+	adc_enable_interrupt(ADC, ADC_ISR_EOC5);
+}
+
+void configure_dac(void){
+  /************************************************************************/
+  /* DAC                                                                  */
+  /************************************************************************/
+  
+  /* Enable clock for DACC */
+  sysclk_enable_peripheral_clock(DACC_ID);
+  
+  /* Reset DACC registers */
+  dacc_reset(DACC_BASE);
+
+  /* Half word transfer mode */
+  dacc_set_transfer_mode(DACC_BASE, 0);
+
+  /* selects channel */
+  dacc_set_channel_selection(DACC_BASE, DACC_CHANNEL);
+
+  /* Enable output channel DACC_CHANNEL */
+  dacc_enable_channel(DACC_BASE, DACC_CHANNEL);
+
+  /* Set up analog current */
+  dacc_set_analog_control(DACC_BASE, DACC_ANALOG_CONTROL);
+ }
+
+/**
+* \brief Application entry point for smc_lcd example.
+*
+* \return Unused (ANSI-C compatibility).
+*/
+int main(void)
+{
+  sysclk_init();
+  board_init();
+
+  uint16_t i ;
+
+  configure_LCD();
+  configure_dac();
+  configure_tc(1000);
+  configure_adc();
+
+  resolucao	 = 100;
+  deltaTeta  = 2.0*PI/((float)resolucao);
+  valorMedio = MAX_AMPLITUDE_ANAG/2.0;
+
+
+  ili93xx_draw_pixmap(0,
+            ILI93XX_LCD_HEIGHT-100-1,
+            240-1,
+            100-1,
+            &image_data_maua[0]);
+  
+
+  while (1) {
+     
+  }
+}
